18ffed0a22f75b4e7b58f13d10927e00226e8bed
James Hazard <james.g.hazard@gmail.com>
2015-04-17 13:13:23 +0200

    initialize

---
+++ landing.php
@@ -0,0 +1,18 @@
+     <p align="center"><img src="assets/cmacc-trans.png" style="width:35%" /></p>
+
+<table><tr><td>Welcome to a subdomain of CommonAccord for ABA-MIT Appathon. 
+<br> Here: <a href="index.php?action=list&file=">Documents</a>:<br><br>
+This is a subset of the docs from <a href="www.commonaccord.org">commonaccord.org</a>.<br>
+
+
+CommonAccord as a "Trusted Member" of the <a href="law.mit.edu">law.MIT.edu</a>  "Open Source Legal Tool & Design Network".<br>
+<br>
+Any person registered for ABA Techshow or registered for the ABA-MIT Online Legal Appathon can sign up for a free CommonAccord walk through, tutorial and question / answer period.
+
+<br><br>We will schedule  one or more such sessions, based on demand, within a week or two following the ABA Techshow.
+
+<br>Many thanks, Jim and Dazza</td><td>
+
+<a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096">Tweets by @CommonAccord</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</td></tr></table>
