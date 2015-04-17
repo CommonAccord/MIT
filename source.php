1aee6d366bcbaedc8f0038c93f6d43ce3b6353ed
HazardJ <jamesghazard@gmail.com>
2015-04-17 13:19:04 +0200

    Update source.php

--- source.php
+++ source.php
@@ -11,11 +11,11 @@ include("header.php");
 //This displays the path, current file name, and provides the edit and show options //
 
 echo "
-<a href=$_SERVER[PHP_SELF]?action=list&file=$rootdir[dirname]/>Org$rootdir[dirname]</a>/<b>$filenameX</b>
-&emsp; &emsp; &emsp; &emsp;  
+<a href=$_SERVER[PHP_SELF]?action=list&file=$rootdir[dirname]/>MIT:$rootdir[dirname]</a>/<b>$filenameX</b>
+&emsp; &emsp; &emsp; &emsp;
 <a href=$_SERVER[PHP_SELF]?action=print&file=$rootdir[dirname]/$filenameX>Print</a>
   &emsp;  &emsp;  &emsp;  
-<a href=https://github.com/CommonAccord/Org/blob/master/Doc/$rootdir[dirname]/$filenameX>GitHub</a>
+<a href=https://github.com/CommonAccord/MIT/blob/master/Doc/$rootdir[dirname]/$filenameX>GitHub</a>
 
 ";
 
